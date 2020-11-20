b80f20974d1968b465ebf22dfb2de39c7344cc4f
Volkan Ozsarac <46993652+volkanozsarac@users.noreply.github.com>
2020-11-20 21:10:09 +0100

    Update Example.py

--- Example.py
+++ Example.py
@@ -1,7 +1,7 @@
 from EzGM import *
 startTime = time()
 # 1.) Initialize the cs_master object for record selection, check which parameters are required for the gmpe you are using.
-cs = cs_master(Tstar = np.arange(0.4,2.4,0.2), gmpe = 'Boore_EtAl_2014', database = 'NGA_W2', pInfo = 1)
+cs = cs_master(Tstar = np.arange(0.4,2.4,0.2), gmpe = 'Boore_EtAl_2014', database = 'EXSIM_Duzce', pInfo = 1)
 
 # 2.) Create target spectrum
 cs.create(site_param = {'vs30': 620}, rup_param = {'rake': 0.0, 'mag': [6.5, 6.0]}, 
@@ -19,13 +19,14 @@ cs.select(nGM=10, selection=2, Sa_def='RotD50', isScaled = 1, maxScale = 4,
 # The simulated spectra and spectra of selected records can be plotted at this stage
 cs.plot(tgt = 0, sim = 1, rec = 1, save = 1, show = 1)
 
-# 4.) Download selected ground motions from NGA-West2 Database [http://ngawest2.berkeley.edu/]
+# 4.) If database == 'NGA_W2' you can first download the records via nga_download method
+# from NGA-West2 Database [http://ngawest2.berkeley.edu/] and then use write method
 cs.nga_download(username = 'example_username', pwd = 'example_password123456')
 
-# 5.) write what you want, the object, selected and scaled time histories
-# For this option you have to have records already inside recs_f\database.zip\database for this option, however,
-# if database == 'NGA_W2' you can first download the records via nga_download method and then use write option.
-cs.write(obj = 1, recs = 1, recs_f = '')
+# 5.) If you have records already inside recs_f\database.zip\database or 
+# downloaded records for database = NGA_W2 case, write whatever you want, 
+# the object itself, selected and scaled time histories
+cs.write(obj = 1, recs = 1, recs_f = os.path.join(os.path.split(os.getcwd())[0],'Records'))
 
 # Calculate the total time passed
 RunTime(startTime)

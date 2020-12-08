0c809c959a60bc72cf8473d1ed4fe5927f389c39
Volkan Ozsarac <46993652+volkanozsarac@users.noreply.github.com>
2020-12-09 02:54:38 +0300

    Update Example.py

--- Example.py
+++ Example.py
@@ -20,14 +20,14 @@ cs.select(nGM = 25, selection = 1, Sa_def = 'RotD50', isScaled = 1, maxScale = 2
 # The simulated spectra and spectra of selected records can be plotted at this stage
 cs.plot(tgt = 0, sim = 1, rec = 1, save = 1, show = 1)
 
-# # 4.) If database == 'NGA_W2' you can first download the records via nga_download method
-# # from NGA-West2 Database [http://ngawest2.berkeley.edu/] and then use write method
-# cs.nga_download(username = 'example_username', pwd = 'example_password123456')
+# 4.) If database == 'NGA_W2' you can first download the records via nga_download method
+# from NGA-West2 Database [http://ngawest2.berkeley.edu/] and then use write method
+cs.nga_download(username = 'example_username', pwd = 'example_password123456')
 
-# # 5.) If you have records already inside recs_f\database.zip\database or 
-# # downloaded records for database = NGA_W2 case, write whatever you want, 
-# # the object itself, selected and scaled time histories
-# cs.write(obj = 1, recs = 1, recs_f = os.path.join(os.path.split(os.getcwd())[0],'Records'))
+# 5.) If you have records already inside recs_f\database.zip\database or 
+# downloaded records for database = NGA_W2 case, write whatever you want, 
+# the object itself, selected and scaled time histories
+cs.write(obj = 1, recs = 1, recs_f = os.path.join(os.path.split(os.getcwd())[0],'Records'))
 
 # Calculate the total time passed
 RunTime(startTime)
